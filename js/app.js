464f64a03f14b0681dc7a7e4295f2523e7314be6
Danielle Bargovic <DBA274@uky.edu>
2017-06-23 22:24:49 -0400

    redesign updates

--- js/app.js
+++ js/app.js
@@ -1,1 +1,20 @@
-(function() {})();
+(function() {
+
+    $(function() {
+        $('#WebMaps').click(function() {
+            window.location = '/webMapGallery.html'
+        })
+        $('#staticMaps').click(function() {
+            window.location = '/staticGallery.html'
+        })
+        $('#AboutMe').click(function() {
+            window.location = '/AboutMe.html'
+        });
+    });
+    // $(function(){
+    //     $('#staticMaps').click(function(){
+    //         window.location='/webMapGallery.html'
+    //     });
+    // });
+
+})();
